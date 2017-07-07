6dc26902e5eb0f2a90e4e76ab25548ca17451719
ELIDJE AKA <elidjeakaemmanuel@gmail.com>
2017-07-07 19:32:22 +0000

    handle to a persistent menu and get started button

--- index.js
+++ index.js
@@ -8,65 +8,81 @@ const bot = new BootBot({
   verifyToken: 'Resto+',
   appSecret: 'a5d353d3d19ff6cc270ed0010b12ab1b'
 });
+// https://restopluss.herokuapp.com/
 
 // SET A GETSTARTED BUTTON
 bot.setGetStartedButton((payload, chat) => {
-  const welcome1 = `Bienvenue à Resto+ ...`;
-  const welcome2 = `Type START or PLAY to join the challenge!`;
-  const options = { typing: true };
-  chat.say(welcome1, options)
-    .then(() => chat.say(welcome2, options));
+
 });
 
 // SET A PERSISTENT MENU
-bot.setPersistentMenu([
-
-  {
-    title: 'Plats',
-    type: 'nested',
-    call_to_actions: [
-      {
-        title: 'Entrée',
-        type: 'postback',
-        payload: 'ENTREE_PAYLOAD'
-      },
-      {
-        title: 'Resistance',
-        type: 'postback',
-        payload: 'RESISTANCE_PAYLOAD'
-      },
-      {
-        title: 'Dessert',
-        type: 'postback',
-        payload: 'DESSERT_PAYLOAD'
-      },
-    ]
-  },
-  {
-    title: 'A Propos',
-    type: 'postback',
-    payload: 'PROPOS_PAYLOAD'
-  },
-  {
-    title: 'Page Facebook',
-    type: 'web_url',
-    url: 'https://www.facebook.com/RestoPluss/'
-  }
-])
-
-
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+bot.setPersistentMenu([
+        {
+          title: 'Plats',
+          type: 'nested',
+          call_to_actions: [
+            {
+              title: 'Entrée',
+              type: 'postback',
+              payload: 'ENTREE_PAYLOAD'
+            },
+            {
+              title: 'Resistance',
+              type: 'postback',
+              payload: 'RESISTANCE_PAYLOAD'
+            },
+            {
+              title: 'Dessert',
+              type: 'postback',
+              payload: 'DESSERT_PAYLOAD'
+            },
+          ]
+        },
+        {
+          title: 'A Propos',
+          type: 'postback',
+          payload: 'PROPOS_PAYLOAD'
+        },
+        {
+          title: 'Page Facebook',
+          type: 'web_url',
+          url: 'https://www.facebook.com/RestoPluss/'
+        }
+      ])
 
 app.post('/', function (req, res) {
     try {
+
         if (req.body) {
+
+
                 console.log(req.body)
                 let requestBody = req.body;
-                if(requestBody.result.resolvedQuery.toLowerCase() == 'entrée' || requestBody.result.resolvedQuery.toLowerCase() == 'entree')
+                if(requestBody.result.resolvedQuery.toLowerCase() == 'entrée' || requestBody.result.resolvedQuery.toLowerCase() == 'entree' || req.body.result.resolvedQuery=='ENTREE_PAYLOAD')
                 {
                   bot.sendTextMessage(req.body.originalRequest.data.sender.id,'cool');
+                  console.log(requestBody.result.resolvedQuery)
+                }
+                if(requestBody.result.resolvedQuery.toLowerCase() == 'resistance' || req.body.result.resolvedQuery=='RESISTANCE_PAYLOAD')
+                {
+                  bot.sendTextMessage(req.body.originalRequest.data.sender.id,'cool');
+                  console.log(requestBody.result.resolvedQuery)
+                }
+                if(requestBody.result.resolvedQuery.toLowerCase() == 'dessert' || req.body.result.resolvedQuery=='DESSERT_PAYLOAD')
+                {
+                  bot.sendTextMessage(req.body.originalRequest.data.sender.id,'cool');
+                  console.log(requestBody.result.resolvedQuery)
+                }
+                if(requestBody.result.resolvedQuery.toLowerCase() == 'démarrer' || requestBody.result.resolvedQuery.toLowerCase() == 'démarrer' || req.body.result.resolvedQuery=='BOOTBOT_GET_STARTED')
+                {
+                  const welcome1 = `Bienvenue à Resto+ ...`;
+                  const welcome2 = `Type START or PLAY to join the challenge!`;
+                  const options = { typing: true };
+                  bot.sendTextMessage(req.body.originalRequest.data.sender.id,welcome1)
+                    .then(() => bot.sendTextMessage(req.body.originalRequest.data.sender.id,welcome2));
                   console.log(requestBody.result.resolvedQuery)
                 }
               }
@@ -81,6 +97,8 @@ app.post('/', function (req, res) {
                 });
             }
         });
+
+
 
 
 
